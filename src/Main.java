67dc02161e074037566b6c35c26098df22c8c00a
Habiba618 <82892084+Habiba618@users.noreply.github.com>
2023-12-13 12:02:42 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int mat, fiz, kim, tur, muz;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Matematik puanınızı giriniz: ");
+        mat = sc.nextInt();
+        System.out.print("Fizik puanınızı giriniz: ");
+        fiz = sc.nextInt();
+        System.out.print("Kimya puanınızı giriniz: ");
+        kim = sc.nextInt();
+        System.out.print("Turkce puanınızı giriniz: ");
+        tur = sc.nextInt();
+        System.out.print("Müzik puanınızı giriniz: ");
+        muz = sc.nextInt();
+        double ortalama;
+        int toplam = 0;
+        int notSayisi = 0;
+
+        if (mat >= 0 && mat <= 100) {
+            toplam += mat;
+            notSayisi++;
+        }
+
+        if (fiz >= 0 && fiz <= 100) {
+            toplam += fiz;
+            notSayisi++;
+        }
+
+        if (kim >= 0 && kim <= 100) {
+            toplam += kim;
+            notSayisi++;
+        }
+
+        if (tur >= 0 && tur <= 100) {
+            toplam += tur;
+            notSayisi++;
+        }
+
+        if (muz >= 0 && muz <= 100) {
+            toplam += muz;
+            notSayisi++;
+        }
+        if (notSayisi > 0) {
+            ortalama = (double) toplam / notSayisi;
+
+            if (ortalama > 55) {
+                System.out.println("Tebrikler..");
+            } else {
+                System.out.println("Kaldınız..");
+            }
+            System.out.println("Ortalamanız: " + ortalama);
+        } else {
+            System.out.println("Geçersiz puan girişi, lütfen 0-100 arasında puan giriniz.");
+        }
+
+
     }
 }
